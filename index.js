7fd13eca9c6846856a03a4b011c5e54e843f6c6c
Florian Uphoff <florianuphoff@secure.mailbox.org>
2019-09-14 23:15:42 +0200

    reports selector, line and origin source file

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const postcss = require('postcss')
 
 module.exports = postcss.plugin('postcss-code-duplication', opts => {
+  const reportStartingLine = opts.reportStartingLine || true
+  const reportOriginSource = opts.reportOriginSource || true
+
   /**
    * opts: 
    * reportStartingLine: true,
@@ -10,16 +13,24 @@ module.exports = postcss.plugin('postcss-code-duplication', opts => {
    */
 
   return (root, result) => {
-
     let selectorList = []
 
+    const consumer = root.source.input.map.consumerCache
+    
     root.walkRules((rule) => {
       const parent = rule.parent
       if (parent.type === 'atrule' && parent.name === 'keyframes') {
         return
       }
 
-      selectorList.push({selector: rule.selector, startsAt: rule.source.start.line})
+      if(reportStartingLine && reportOriginSource) {
+        selectorList.push({selector: rule.selector, startsAt: rule.source.start.line, origin: consumer.originalPositionFor({ line: rule.source.start.line, column: rule.source.start.column }).source})
+      } else if(reportStartingLine && !reportOriginSource) {
+        selectorList.push({selector: rule.selector, startsAt: rule.source.start.line, origin: ''})
+      } else if(!reportStartingLine && reportOriginSource) {
+        selectorList.push({selector: rule.selector, startsAt: 0, origin: consumer.originalPositionFor({ line: rule.source.start.line, column: rule.source.start.column }).source})
+      }
+
       
     })
     result.warn('Gathered statistics', selectorList)
